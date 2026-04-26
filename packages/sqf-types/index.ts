4d8142078b9130d6460486118ab066f5bf28d5f4
James Garrick <jamesgarrick1027@gmail.com>
2026-04-26 15:01:56 -0400

    tsx working on sqf-types

--- packages/sqf-types/index.ts
+++
@@ -1,4 +0,0 @@
-export { cfgNode } from "./src/cfg-runtime";
-export { cfgWeapons, weapons } from "./src/cfg-weapons";
-export { cfgWeaponsItems, weaponsItems } from "./src/cfg-weapons-items";
-export { cfgMagazines, magazines } from "./src/cfg-magazines";
