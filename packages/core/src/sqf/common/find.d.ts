52b57a31e2095387064e21b42ec7b9237b59f154
James Garrick <jamesgarrick1027@gmail.com>
2026-04-29 12:26:55 -0400

    add find commands

--- packages/core/src/sqf/common/find.d.ts
+++ packages/core/src/sqf/common/find.d.ts
@@ -17,3 +17,63 @@ export function find(source: string, search: string, startOffset?: number): numb
  * * @see {@link https://community.bistudio.com/wiki/find find}
  */
 export function find<T>(source: T[], search: T): number;
+
+/**
+ * Returns the zero-based index of the first
+ * element in array A that also exists in array B
+ *
+ * @since 2.10
+ *
+ * @param source Array to search in.
+ * @param search Array to search for.
+ *
+ * @returns The zero-based index of the first found item, or -1 if not found.
+ * * @see {@link https://community.bistudio.com/wiki/findAny findAny}
+ */
+export function findAny(source: Array<any>, search: Array<any>): number;
+
+// typed enrichment
+/**
+ * Returns the zero-based index of the first
+ * element in array A that also exists in array B
+ *
+ * @since 2.10
+ *
+ * @param source Array to search in.
+ * @param search Array to search for.
+ *
+ * @returns The zero-based index of the first found item, or -1 if not found.
+ * * @see {@link https://community.bistudio.com/wiki/findAny findAny}
+ */
+export function findAny<T>(source: T[], search: T[]): number;
+
+/**
+ * Searches for an element within array for which the code evaluates to true.
+ * Returns the zero-based index on success or -1 if not found.
+ *
+ * @since 1.82
+ *
+ * @param array Array to search in.
+ * @param code Function to test each element.
+ *
+ * @returns number - zero-based position of the first array element for which the code evaluate to true, -1 if not found
+ *
+ * @see {@link https://community.bistudio.com/wiki/findIf findIf}
+ */
+export function findIf(array: Array<any>, code: (item: any) => boolean): number;
+
+// typed
+/**
+ * Searches for an element within array for which the code evaluates to true.
+ * Returns the zero-based index on success or -1 if not found.
+ *
+ * @since 1.82
+ *
+ * @param array Array to search in.
+ * @param code Function to test each element.
+ *
+ * @returns number - zero-based position of the first array element for which the code evaluate to true, -1 if not found
+ *
+ * @see {@link https://community.bistudio.com/wiki/findIf findIf}
+ */
+export function findIf<T>(array: Array<T>, code: (item: T) => boolean): number;
