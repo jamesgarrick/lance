c7c2e09d8668d84e95bc6c5bcd14d3fcb61622ee
James Garrick <jamesgarrick1027@gmail.com>
2026-04-30 17:57:08 -0400

    fix core/src exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,11 +3,9 @@ export * from "./sqf/index"
 export * from "./cfg/cfg-runtime";
 export * from "./cfg/config-file";
 export * from "./cfg/cfg-glasses";
-export * from "./cfg/cfg-weapons";
 export * from "./cfg/cfg-weapons-equipment";
 export * from "./cfg/cfg-weapons-items";
 export * from "./cfg/cfg-weapons-vehicle-weapons";
-export * from "./cfg/cfg-weapons-weapons";
 export * from "./cfg/cfg-magazines";
 export * from "./cfg/cfg-vehicles-animals";
 export * from "./cfg/cfg-vehicles-backpacks";
